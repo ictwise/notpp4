e63a0574046041dc47308f1daf739b06f80b135a
ictwise <ictwise@gmail.com>
2023-01-18 15:01:25 +0000

    updated webhookhandler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
+import stripe
 
 from .models import Order, OrderLineItem
 from products.models import Product
