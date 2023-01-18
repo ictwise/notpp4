dc7cd569bc6664da3753a0363efa94c2b6d87484
ictwise <ictwise@gmail.com>
2023-01-18 15:48:51 +0000

    removed DEVELOPMENT from settings

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51MMIerG8VXdhRAoCcwm3v6l94ead0XfQiABc
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51MMIerG8VXdhRAoCoIfWaqIwUebRAqdQmjKspOAdPJdxwG1NV9f45gREJfMNqyfqYGFx0yc6Yr5edTHP1TMD8tUy00oeHzyKfa"
 os.environ["SECRET_KEY"] = "mehtni6-n1#q7^1gkf-=!e)caamrv8kyf5_f4t_9dvazixlh+*"
 os.environ["STRIPE_WH_SECRET"] = "whsec_MkzV0UlyJpEZfDab1f6yemBkg88CLnen"
-os.environ.setdefault('DEVELOPMENT', '1')
+
