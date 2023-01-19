2b50af76bb4082bc3634c0c8643cda6acd9e0587
ictwise <ictwise@gmail.com>
2023-01-19 17:16:53 +0000

    fix: blog.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import BlogView
 
 urlpatterns = [
-    path('', views.view_blog, name='blog'),
+    path('', BlogView.as_view(), name='blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def view_blog(request):
-    return render(request, 'blog.html', {})
+# def view_blog(request):
+#    return render(request, 'blog/blog.html', {})
+
+class BlogView(ListView):
+    model = Post
+    template_name = 'blog.html'
