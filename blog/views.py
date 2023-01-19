8abf3d100fa016f13d349dc93ff637afb6397ee2
ictwise <ictwise@gmail.com>
2023-01-19 20:51:09 +0000

    fix: typo views.py blog

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from .models import Post
 
 
-
 def BlogView(request):
     context = {
-        'posts' : Post.objects.all()
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
    
