c6c076347288645ed3f022f09ec867ba2b4322aa
ictwise <ictwise@gmail.com>
2023-01-25 16:06:03 +0000

    comments added to post detail

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     body = models.TextField()
-    date_added = models.DateField(auto_now=True)
+    date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return '%s - %s' % (self.post.title, self.name)
