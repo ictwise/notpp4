10d4acf5fcbb3ebc0317f52751457c53edc67c06
ictwise <ictwise@gmail.com>
2023-02-02 09:19:30 +0000

    debug set

--- ecobiketech/settings.py
+++ ecobiketech/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = int(os.environ.get("DEVELOPMENT", default=0))
 
 ALLOWED_HOSTS = ['ecobiketech.herokuapp.com', 'localhost']
 
